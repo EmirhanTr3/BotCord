d99373849a2d43ec74f6256426eea71860964e9b
EmirhanTr3 <emirhaneren20072010@gmail.com>
2024-05-11 17:44:06 +0300

    fixed message structure

--- src/renderer/components/message.tsx
+++ src/renderer/components/message.tsx
@@ -3,14 +3,18 @@ import { BotBadge, PFP } from "."
 
 export default function MessageC({ message }: { message: Message }) {
     return <div id="message" className="hover">
-        <PFP src={message.author.avatar} height={42} width={42}/>
-        <div id="content">
-            <div>
-                <p id="username" style={{color: message.author.displayColor}}>{message.author.displayName}</p>
-                {(message.author.bot || message.author.webhook) && <BotBadge member={message.author} />}
-                <p id="time">{message.createdAt}</p>
+        <div id="messagecontent">
+            <PFP src={message.author.avatar} height={42} width={42}/>
+            <div id="content">
+                <div>
+                    <p id="username" style={{color: message.author.displayColor}}>{message.author.displayName}</p>
+                    {(message.author.bot || message.author.webhook) && <BotBadge member={message.author} />}
+                    <p id="time">{message.createdAt}</p>
+                </div>
+                <p id="msg">
+                    <p id="msgcontent">{message.content}</p>
+                </p>
             </div>
-            <p id="msg">{message.content}</p>
         </div>
     </div>
 }
