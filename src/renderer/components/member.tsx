42fab914869c0d1f6708c45e37df6353afdd411c
aspectx <67403935+aspectxlol@users.noreply.github.com>
2024-05-20 19:34:04 +0700

    modals

--- src/renderer/components/member.tsx
+++ src/renderer/components/member.tsx
@@ -1,11 +1,19 @@
 import { Member } from "src/shared/types";
 import { BotBadge, PFP, UserStatus } from ".";
+import { useState } from "react";
 
 export default function MemberC({ member }: { member: Member }) {
-    return <div id="member" className={"hover" + (member.status == "offline" ? " offline" : "")}>
-        <PFP src={member.avatar} height={32} width={32} />
-        <UserStatus height={10} width={10} status={member.status}/>
-        <p id="username" style={{color: member.displayColor}}>{member.displayName}</p>
-        {(member.bot || member.webhook) && <BotBadge member={member} />}
-    </div>
+    const [Open, setOpen] = useState(false)
+
+    return <>
+        <div id={`profile-modal`} className={Open? ``: "hidden"}>
+            working modal
+        </div>
+        <div id="member" className={"hover" + (member.status == "offline" ? " offline" : "")} role="button" onClick={() => setOpen(!Open)}>
+            <PFP src={member.avatar} height={32} width={32} />
+            <UserStatus height={10} width={10} status={member.status}/>
+            <p id="username" style={{color: member.displayColor}}>{member.displayName}</p>
+            {(member.bot || member.webhook) && <BotBadge member={member} />}
+        </div>
+    </>
 }
